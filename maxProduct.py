e2344548a0d79613418a96691be90b07acf24c19
Ozer, Ozdal <ozerozdal@gmail.com>
2024-05-26 16:41:59 -0400

    updating maxProduct

--- maxProduct.py
+++ maxProduct.py
@@ -7,19 +7,25 @@
 class Solution:
     def maxProduct(self, nums: list) -> int:
         nums.sort()
-        return max(nums[0] * nums[-1] * nums[-2], nums[-1] * nums[-2] * nums[-3])
+        return max(nums[0] * nums[1] * nums[-1], nums[-1] * nums[-2] * nums[-3])
     
 
 def testSolution1():
     nums=[-10, -9, 5, 6, 7, 8, -2, -4, -1, 2]
     sol = Solution()
-    return sol.maxProduct(nums)
+    return sol.maxProduct(nums) # returns 720
 
 def testSolution2():
-    nums=[-10, -9, 8, -2, -4, -1, 2]
+    nums=[-10, -9, -2, -4, -1, 2]
     sol = Solution()
-    return sol.maxProduct(nums)
+    return sol.maxProduct(nums) # returns 180
+
+def testSolution3():
+    nums=[2, 3, 5, 7]
+    sol = Solution()
+    return sol.maxProduct(nums) # return 105
 
 
 print(testSolution1())
 print(testSolution2())
+print(testSolution3())
